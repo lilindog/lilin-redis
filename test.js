4b575835b45676c34b77f884a593b4eda10707dd
lilindog <lilin@lilin.site>
2019-04-13 00:52:22 +0800

    新的handler初步测试通过，尤其是之前诟病的*阵列部分的解析

--- test.js
+++ test.js
@@ -5,11 +5,21 @@ const fs = require("fs");
 let redis = new Redis("127.0.0.1", "6379", "8612");
 
 
-let str = fs.readFileSync("./data.text");
+// let str = fs.readFileSync("./data.text");
 
 !async function(){
+    try{
 
-    //await redis.lpush("task", str, str, str, str);
-    console.log(await redis.lrange("task", 0, -1));
+        //await redis.lpush("task", str, str, str, str);
+        // console.log(await redis.lrange("task", 0, -1));
+        // console.log("test输出的数据： " + await redis.lrange("task"));
+        await redis.lrange("task")
+
+
+
+    }catch(err){
+        console.log("test数据出错：");
+        console.log(err);
+    }
 
 }();
