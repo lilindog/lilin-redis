741836f8ec2be84b17823827fde60a1037ab370c
lilindog <lilin@lilin.site>
2019-04-13 01:25:41 +0800

    修改*处理下$的时候，回调参数替换RESP字符错误替换的问题

--- test.js
+++ test.js
@@ -12,8 +12,8 @@ let redis = new Redis("127.0.0.1", "6379", "8612");
 
         //await redis.lpush("task", str, str, str, str);
         // console.log(await redis.lrange("task", 0, -1));
-        // console.log("test输出的数据： " + await redis.lrange("task"));
-        await redis.lrange("task")
+        console.log(Array.isArray( await redis.lrange("task55")));
+        // await redis.lrange("task55")
 
 
 
