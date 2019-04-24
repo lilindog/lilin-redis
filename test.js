a40f49d00d64069044885f27c9c918fdd35ac86a
lilin123 <lilindog@qq.com>
2019-04-24 12:45:47 +0800

    修复了读取含有换行符数据时候的解析问题

--- test.js
+++ test.js
@@ -5,7 +5,16 @@ let redis = new Redis("127.0.0.1", "6379", "8612");
 !async function(){
     try{
 
-        console.log(await redis.lset("test", 0, "lilin"));
+        // console.log(await redis.lset("test", 0, "lilin"));
+        // await redis.set("key1", `
+        // 123
+        // 456
+        // 789
+        // `);
+        let res = await redis.lrange("test1");
+        console.log(res);
+        console.log(res[0]);
+        // console.log("getkey1返回为: " + await redis.get("key1"));
 
 
     }catch(err){
@@ -15,14 +24,14 @@ let redis = new Redis("127.0.0.1", "6379", "8612");
 
 }();
 
-async function del(v){
-    let arr = await redis.lrange("test");
-    for(let i = 0; i < arr.length; i++)
-    {
-        if (arr[i] === v){
-            await redis.lset("test", i, "---");
-        }
-    }
-    await redis.lrem("test", 0, "---");
+// async function del(v){
+//     let arr = await redis.lrange("test");
+//     for(let i = 0; i < arr.length; i++)
+//     {
+//         if (arr[i] === v){
+//             await redis.lset("test", i, "---");
+//         }
+//     }
+//     await redis.lrem("test", 0, "---");
     
-}
+// }
